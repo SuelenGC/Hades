4996cd06d1f07e6cb5d80dbec4035604234f5ea1
karynzv <karyn.saz@gmail.com>
2015-05-06 14:30:01 -0300

    Fianlizei filtro inicial, mandando variaveis[@karynzv,@jackiewalker]

--- WebContent/resources/js/hades_intro.js
+++ WebContent/resources/js/hades_intro.js
@@ -1,40 +1,23 @@
-
-$('#filtro_anterior').click(function(){
-	$('#pago').trigger('click');
+$('#pago').click(function(){
+	$('#intro_caixa_botoes').children().remove();
+	$('#titulo_caixa').children().remove();
+	var titulo = '<h1>Tipo de Contratação</h1>';
+    var url = window.location.href + '/contratante/formulario?tipoOperacao=1';
+	var botao1 = '<button type="button" onClick="destino_final(1, \''+ url+'\');" id="contratacao_normal" class="btn btn-block" style="margin-top:10%;">1. Contratação <b><u>n</u></b>ormal</button>';
+    var botao2 = '<button type="button" onClick="destino_final(2, \''+ url+'\');" id="corpo_vindo_de_fora" class="btn btn-block" style="margin-top:5%;">2. Corpo vindo de <b><u>f</u></b>ora</button>';
+    var botao3 = '<button type="button" onClick="destino_final(3, \''+ url+'\');" id="membro" class="btn btn-block" style="margin-top:5%;">3. <b><u>M</u></b>embro</button>';
+    var botao4 = '<button type="button" onClick="destino_final(4, \''+ url+'\');" id="semi_intacto" class="btn btn-block" style="margin-top:5%;margin-bottom: 5%;">4. <b><u>S</u></b>emi intacto</button>';
+    $('#titulo_caixa').append(titulo);
+	$('#intro_caixa_botoes').append(botao1).append(botao2).append(botao3).append(botao4);
 });
 $('#cancelar').click(function(){
-	$('#pago').trigger('click');
-});
-$('#pago').click(function(){
-	$('#intro_caixa_botoes').children().remove();
-	$('#titulo_caixa').children().remove();
-	var titulo = '<h1>Tipo de Contratação</h1>';
-    var url = 'tipoOperacao=1';
-	var botao1 = '<button type="button" onClick="destino_final(1, \''+ url+'\');" id="contratacao_normal" class="btn btn-block" style="margin-top:10%;">1. Contratação <b><u>n</u></b>ormal</button>';
-    var botao2 = '<button type="button" onClick="destino_final(2, \''+ url+'\');" id="corpo_vindo_de_fora" class="btn btn-block" style="margin-top:5%;">2. Corpo vindo de <b><u>f</u></b>ora</button>';
-    var botao3 = '<button type="button" onClick="destino_final(3, \''+ url+'\');" id="membro" class="btn btn-block" style="margin-top:5%;">3. <b><u>M</u></b>embro</button>';
-    var botao4 = '<button type="button" onClick="destino_final(4, \''+ url+'\');" id="semi_intacto" class="btn btn-block" style="margin-top:5%;margin-bottom: 5%;">4. <b><u>S</u></b>emi intacto</button>';
-    $('#titulo_caixa').append(titulo);
-	$('#intro_caixa_botoes').append(botao1).append(botao2).append(botao3).append(botao4);
-});
-$('#pago').click(function(){
-	$('#intro_caixa_botoes').children().remove();
-	$('#titulo_caixa').children().remove();
-	var titulo = '<h1>Tipo de Contratação</h1>';
-    var url = 'tipoOperacao=1';
-	var botao1 = '<button type="button" onClick="destino_final(1, \''+ url+'\');" id="contratacao_normal" class="btn btn-block" style="margin-top:10%;">1. Contratação <b><u>n</u></b>ormal</button>';
-    var botao2 = '<button type="button" onClick="destino_final(2, \''+ url+'\');" id="corpo_vindo_de_fora" class="btn btn-block" style="margin-top:5%;">2. Corpo vindo de <b><u>f</u></b>ora</button>';
-    var botao3 = '<button type="button" onClick="destino_final(3, \''+ url+'\');" id="membro" class="btn btn-block" style="margin-top:5%;">3. <b><u>M</u></b>embro</button>';
-    var botao4 = '<button type="button" onClick="destino_final(4, \''+ url+'\');" id="semi_intacto" class="btn btn-block" style="margin-top:5%;margin-bottom: 5%;">4. <b><u>S</u></b>emi intacto</button>';
-    $('#titulo_caixa').append(titulo);
-	$('#intro_caixa_botoes').append(botao1).append(botao2).append(botao3).append(botao4);
-});
-
+	window.location = window.location.href;
+})
 $('#gratuito').click(function(){
 	$('#intro_caixa_botoes').children().remove();
 	$('#titulo_caixa').children().remove();
 	var titulo = '<h1>Tipo de Contratação</h1>';
-	var url = "tipoOperacao=2";
+	var url = window.location.href + "/contratante/formulario?tipoOperacao=2";
 	var botao1 = '<button type="button" onclick="contratacoes_gratuito(1, \''+ url+'\');"  id="social" class="btn btn-block" style="margin-top:10%;">1. <b><u>S</u></b>ocial</button>';
     var botao2 = '<button type="button" onclick="contratacoes_gratuito(2, \''+ url+'\');"  id="membro" class="btn btn-block" style="margin-top:5%;">2. <b><u>M</u></b>embro</button>';
     var botao3 = '<button type="button" onclick="contratacoes_gratuito(3, \''+ url+'\');"  id="desconhecido" class="btn btn-block" style="margin-top:5%;margin-bottom:5%;">3. <b><u>D</u></b>esconhecido</button>';
@@ -44,11 +27,12 @@ $('#gratuito').click(function(){
 
 $('#registro_de_obito').click(function(){
 	/* FINAL */
-	var url = "tipoOperacao=3";
+	var url = window.location.href + "/contratante/formulario?tipoOperacao=3";
+	window.location.href = url;
 });
 
 function destino_final(valor, urlAnterior){
-	var	url = urlAnterior + "&tipoContratacao="+valor;
+	var	url = urlAnterior + "&tipoContratacao=" + valor;
 	$('#intro_caixa_botoes').children().remove();
 	$('#titulo_caixa').children().remove();
 	var titulo = '<h1>Destino Final</h1>';
@@ -73,7 +57,6 @@ function inicio(){
 	$('#intro_caixa_botoes').append(botao1).append(botao2).append(botao3).append(botao4);
 	var	url = urlAnterior + "&tipoContratacao="+valor;
 	var titulo = '<h1>Destino Final</h1>';
-
 	var botao1 = '<button type="button" onclick="destinoFinal(1, \''+ url+'\');" id="sepultamento" class="btn btn-block" style="margin-top:10%;">1. <b><u>S</u></b>epultamento</button>';
     var botao2 = '<button type="button" onclick="destinoFinal(2, \''+ url+'\');" id="cremacao" class="btn btn-block" style="margin-top:5%;margin-bottom:5%;">2. <b><u>C</u></b>remação</button>';
     $('#intro_caixa_botoes').append(botao1).append(botao2); 
@@ -82,13 +65,15 @@ function inicio(){
 
 function contratacoes_gratuito(valor, urlAnterior)
 {
-	var url = urlAnterior + "&tipoContratacao=" + valor +"&destFinal=1";
+	var url = urlAnterior + "&tipoContratacao=" + valor +"&destinoFinal=1";
+	window.location.href = url;
 	/* FINAL */
 }
 
 function destinoFinal(valor, urlAnterior)
 {
-	var url = urlAnterior + "&destFinal=" + valor;
+	var url = urlAnterior + "&destinoFinal=" + valor;
+	window.location.href = url;
 	/* FINAL */
 }
 
